d196efcc8b518bac29525f5a30b9eb65add0b994
robotwalk <161452668+robotwalk@users.noreply.github.com>
2024-03-16 20:18:39 +0100

    add basic treesitter config to nvim configuration (#5)
    * init
    
    * add basic treesitter config

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -64,8 +64,25 @@ myPlugins = {
         vim.o.hidden = true
         require('nvim-terminal').setup()
     end,
-  }
+  },
   ---------------- END nvim-terminal------------------------------
+  ---------------- nvim-treesitter ------------------------------
+ {
+   "nvim-treesitter/nvim-treesitter",
+   build = ":TSUpdate",
+   config = function () 
+     local configs = require("nvim-treesitter.configs")
+     configs.setup({
+         ensure_installed = {  "c", "lua", "vim", "vimdoc", "query",  "html", "dockerfile", 
+                               "bash", "awk", "go", "gotmpl", "hcl", "jq", "json", "latex", "luadoc", 
+                               "make", "markdown", "python", "regex", "toml", "xml", "yaml"},
+         sync_install = false,
+         highlight = { enable = true },
+         indent = { enable = true },  
+       })
+   end,
+ },
+  ---------------- END nvim-treesitter ------------------------------
 }
 
 require("lazy").setup(
