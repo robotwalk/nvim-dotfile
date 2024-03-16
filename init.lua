f0b0a4460b1ed6e2e2bc0e39d3883befeb5957f3
robotwalk <161452668+robotwalk@users.noreply.github.com>
2024-03-16 20:44:28 +0100

    Add lualine, set theme and disable arrowkeys (#6)

--- init.lua
+++ init.lua
@@ -3,3 +3,8 @@ require("user.keymaps")
 require("user.lazy")
 -- otherwise nvim-tree wont work
 require("nvim-tree").setup {}
+require('lualine').setup {
+  options = {
+    theme = 'gruvbox_dark'
+  }
+}
